80e376169317445838806814fef8f6646c3cf576
Yotam <wntrb1st@gmail.com>
2026-06-20 23:38:59 +0300

    bugfix helperFuncs

--- src/controllers/Funcs/helperFunctions.js
+++ src/controllers/Funcs/helperFunctions.js
@@ -3,6 +3,6 @@ module.exports = {
     isregisterFieldEmpty(firstName, email, lastName, username, password, role) { 
         return !firstName || !lastName || !username || !password || !role || !email
                firstName.trim() === "" || lastName.trim() === "" ||
-               username.trim() === "" || password.trim() === "" || role.trim() === "" || !email.find(@); 
+               username.trim() === "" || password.trim() === "" || role.trim() === "" || !email.includes(`@`); 
     }
 };
